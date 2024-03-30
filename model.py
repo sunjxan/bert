0fb11eb2beab46595243a27951e1f3f399f186c3
sunjian <420448185@qq.com>
2024-03-30 17:16:00 +0000

    update model.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- model.py
+++ model.py
@@ -46,3 +46,66 @@ class Decoder(nn.Module):
 
     def forward(self):
         pass
+
+import copy
+import torch.nn as nn
+
+def _get_clones(module, N):
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class LayerNorm(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+class EncoderLayer(nn.Module):
+    def __init__(self, self_attn):
+        super().__init__()
+        self.self_attn = self_attn
+
+    def forward(self):
+        pass
+
+class Encoder(nn.Module):
+    def __init__(self, encoder_layer, num_layers, norm=None):
+        super().__init__()
+        self.layers = _get_clones(encoder_layer, num_layers)
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(self):
+        pass
+
+class DecoderLayer(nn.Module):
+    def __init__(self, self_attn):
+        super().__init__()
+        self.self_attn = self_attn
+
+    def forward(self):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self, decoder_layer, num_layers, norm=None):
+        super().__init__()
+        self.layers = _get_clones(decoder_layer, num_layers)
+        self.num_layers = num_layers
+        self.norm = norm
+
+    def forward(self):
+        pass
+
+class SublayerConnection(nn.Module):
+    def __init__(self, size, dropout=.5, norm_first=True):
+        super().__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+        self.norm_ffirst = norm_first
+
+    def forward(self, x, sublayer):
+        if self.norm_first:
+            x = x + self.dropout(sublayer(self.norm(x)))
+        else:
+            x = self.norm(x + self.dropout(sublayer(x)))
+        return x
