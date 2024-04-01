5c7c6b7d353ebc48e5f36254aec1bf4034f36c32
sunjian <420448185@qq.com>
2024-04-01 16:57:53 +0000

    update model.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import copy
+import math
 import torch
 import torch.nn as nn
 
@@ -12,16 +13,16 @@ class LayerNorm(nn.Module):
         self.eps = eps
         self.weight = nn.Parameter(torch.empty(self.shape))
         self.bias = nn.Parameter(torch.empty(self.shape))
-        self.reset_parameters()
-    
-    def reset_parameters(self):
-        nn.init.ones_(self.weight)
-        nn.init.zeros_(self.bias)
+        self._reset_parameters()
 
     def forward(self, x):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
         return self.weight * (x - mean) / (std + self.eps) + self.bias
+    
+    def _reset_parameters(self):
+        nn.init.ones_(self.weight)
+        nn.init.zeros_(self.bias)
 
 class SublayerConnection(nn.Module):
     def __init__(self, d_model, dropout=.1, norm_first=True):
@@ -87,7 +88,7 @@ class Decoder(nn.Module):
             x = self.norm(x)
         return x
 
-class Transformer(nn.Module):
+class EncoderDecoder(nn.Module):
     def __init__(self, encoder, decoder):
         super().__init__()
         self.encoder = encoder
@@ -103,3 +104,59 @@ class Transformer(nn.Module):
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
+
+def attention(query, key, value, mask=None, dropout=None):
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, d_model, num_heads, dropout=.1):
+        super().__init__()
+        assert d_model % num_heads == 0
+        self.d_k = d_model // num_heads
+        self.num_heads = num_heads
+        self.linears = _get_clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+
+        query, key, value = [
+            lin(x).view(nbatches, -1, self.num_heads, self.d_k).transpose(1, 2)
+            for lin, x in zip(self.linears, (query, key, value))
+        ]
+
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.num_heads * self.d_k)
+
+        return self.linears[-1](x)
+
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(self.w_1(x).relu()))
+
+def build_transformer(d_model=512, d_ff=2048, h=8, N=6, dropout=.1):
+    c = copy.deepcopy
+    attn = MultiHeadedAttention(d_model, h)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N)
+    )
+    return model
