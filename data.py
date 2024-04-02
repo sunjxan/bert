925b48221efec4a57effb4201d5613b438450564
sunjian <420448185@qq.com>
2024-04-02 12:37:09 +0000

    add data.py.
    Signed-off-by: sunjian <420448185@qq.com>

---
+++ data.py
@@ -0,0 +1,27 @@
+from torch.utils.data import Dataset
+
+import config
+
+class MyDataset(Dataset):
+    def __init__(self):
+        super().__init__()
+        with open(config.src_input_file, 'r', encoding='UTF8') as f:
+            self._src_sents = list(f)
+        with open(config.tgt_input_file, 'r', encoding='UTF8') as f:
+            self._tgt_sents = list(f)
+
+        sorted_index = sorted(range(len(src_sents)), key=lambda i: len(src_sents[i]))
+        self._src_sents = [self._src_sents[i] for i in sorted_index]
+        self._tgt_sents = [self._tgt_sents[i] for i in sorted_index]
+        self._dataset_size = min(len(self._src_sents), len(self._tgt_sents))
+
+    def __len__(self):
+        return self._dataset_size
+
+    def __getitem__(self, index):
+        if index >= 0 and index < self.__len__():
+            src_sent = self._src_sents[index]
+            tgt_sent = self._tgt_sents[index]
+            return src_sent, tgt_sent
+        return
+        
