ef2f4db7cf5684f91f8794bee0ba95233dc6466d
sunjian <420448185@qq.com>
2024-04-02 11:58:47 +0000

    add config.py.
    Signed-off-by: sunjian <420448185@qq.com>

---
+++ config.py
@@ -0,0 +1,6 @@
+src_input_file = './data/corpus_eng.txt'
+src_vocab_size = 32000
+src_model_prefix = 'eng'
+tgt_input_file = './data/corpus_chn.txt'
+tgt_vocab_size = 32000
+tgt_model_prefix = 'chn'
