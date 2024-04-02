71aa981064e028b33e960a2629fb0dbc7f7aa32a
sunjian <420448185@qq.com>
2024-04-02 12:45:40 +0000

    update data.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- data.py
+++ data.py
@@ -6,17 +6,17 @@ class MyDataset(Dataset):
     def __init__(self):
         super().__init__()
         with open(config.src_input_file, 'r', encoding='UTF8') as f:
-            self._src_sents = list(f)
+            self._src_sents = list(map(lambda x: x.strip(), f))
         with open(config.tgt_input_file, 'r', encoding='UTF8') as f:
-            self._tgt_sents = list(f)
+            self._tgt_sents = list(map(lambda x: x.strip(), f))
 
         sorted_index = sorted(range(len(src_sents)), key=lambda i: len(src_sents[i]))
         self._src_sents = [self._src_sents[i] for i in sorted_index]
         self._tgt_sents = [self._tgt_sents[i] for i in sorted_index]
-        self._dataset_size = min(len(self._src_sents), len(self._tgt_sents))
+        self._size = min(len(self._src_sents), len(self._tgt_sents))
 
     def __len__(self):
-        return self._dataset_size
+        return self._size
 
     def __getitem__(self, index):
         if index >= 0 and index < self.__len__():
