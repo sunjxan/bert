29de10ba98bb8fda0c219454903a122c26c9a339
sunjian <420448185@qq.com>
2024-04-03 06:14:25 +0000

    update tokenize.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- tokenize.py
+++ tokenize.py
@@ -6,21 +6,9 @@ import config
 def train(input_file, model_prefix, vocab_size, model_type, character_coverage):
     arg = f'--input={input_file} --model_prefix={model_prefix} --vocab_size={vocab_size} \
 --model_type={model_type} --character_coverage={character_coverage} \
---pad_id={config.padding_id} --unk_id={config.unknown_id} \
---bos_id={config.bos_id} --eos_id={config.eos_id}'
+--pad_id=0 --unk_id=1 --bos_id=2 --eos_id=3'
     spm.SentencePieceTrainer.Train(arg)
 
-def load_tokenizers():
-    src_model_path = config.src_model_prefix + '.model'
-    if not os.path.exists(src_model_path):
-        train(config.src_input_file, config.src_model_prefix, config.src_vocab_size, config.src_model_type, config.src_character_coverage)
-    src_sp = spm.SentenPieceProcessor()
-    src_sp.Load(src_model_path)
-
-    tgt_model_path = config.tgt_model_prefix + '.model'
-    if not os.path.exists(tgt_model_path):
-        train(config.tgt_input_file, config.tgt_model_prefix, config.tgt_vocab_size, config.tgt_model_type, config.tgt_character_coverage)
-    tgt_sp = spm.SentenPieceProcessor()
-    tgt_sp.Load(tgt_model_path)
-    
-    return src_sp, tgt_sp
+if __name__ == '__main__':
+    train(config.src_input_file, config.src_model_prefix, config.src_vocab_size, config.src_model_type, config.src_character_coverage)
+    train(config.tgt_input_file, config.tgt_model_prefix, config.tgt_vocab_size, config.tgt_model_type, config.tgt_character_coverage)
