07b007b79a4d2830e1c374a2e4f544e9d3a4d397
sunjian <420448185@qq.com>
2024-04-03 06:12:56 +0000

    update config.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+src_input_file = './data/corpus.en'
 src_vocab_size = 32000
+src_model_prefix = 'en'
+src_model_type = 'bpe'
+src_character_coverage = 1
+
+tgt_input_file = './data/corpus.zh'
 tgt_vocab_size = 32000
-src_model_prefix = 'eng'
-tgt_model_prefix = 'chn'
+tgt_model_prefix = 'zh'
+tgt_model_type = 'bpe'
+tgt_character_coverage = .9995
