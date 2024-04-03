036c0d2e4cd96e4a0ee0d64b38ac1a23a5c47c83
sunjian <420448185@qq.com>
2024-04-03 07:46:08 +0000

    update config.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import torch
+
 src_input_file = './data/train/corpus.en'
 src_vocab_size = 32000
 src_model_prefix = 'en'
@@ -9,3 +11,7 @@ tgt_vocab_size = 32000
 tgt_model_prefix = 'zh'
 tgt_model_type = 'bpe'
 tgt_character_coverage = .9995
+
+batch_size = 32
+
+device = torch.device('cpu')
