d5aadd38287cdd379262a508dca76cf61a8e3299
sunjian <420448185@qq.com>
2024-04-03 07:10:20 +0000

    update data.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- data.py
+++ data.py
@@ -2,17 +2,24 @@ from torch.utils.data import Dataset
 
 import config
 
-class MyDataset(Dataset):
-    def __init__(self):
-        super().__init__()
-        with open(config.src_input_file, 'r', encoding='UTF8') as f:
-            self._src_sents = list(map(lambda x: x.strip(), f))
-        with open(config.tgt_input_file, 'r', encoding='UTF8') as f:
-            self._tgt_sents = list(map(lambda x: x.strip(), f))
+def load_tokenizers():
+    src_model_path = config.src_model_prefix + '.model'
+    tokenizer_src = spm.SentenPieceProcessor()
+    tokenizer_src.Load(src_model_path)
 
-        sorted_index = sorted(range(len(src_sents)), key=lambda i: len(src_sents[i]))
-        self._src_sents = [self._src_sents[i] for i in sorted_index]
-        self._tgt_sents = [self._tgt_sents[i] for i in sorted_index]
+    tgt_model_path = config.tgt_model_prefix + '.model'
+    tokenizer_tgt = spm.SentenPieceProcessor()
+    tokenizer_tgt.Load(tgt_model_path)
+    
+    return tokenizer_src, tokenizer_tgt
+
+class MyDataset(Dataset):
+    def __init__(self, src_input_file, tgt_input_file):
+        super().__init__()
+        with open(src_input_file, 'r', encoding='UTF8') as f:
+            self._src_sents = list(map(lambda x: x.strip(), f))
+        with open(tgt_input_file, 'r', encoding='UTF8') as f:
+            self._tgt_sents = list(map(lambda x: x.strip(), f))
         self._size = min(len(self._src_sents), len(self._tgt_sents))
 
     def __len__(self):
