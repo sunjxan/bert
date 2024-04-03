223d39b7b445441d4c3ee792ee3cdb6bef61e195
sunjian <420448185@qq.com>
2024-04-03 06:18:07 +0000

    update config.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-src_input_file = './data/corpus.en'
+src_input_file = './data/train/corpus.en'
 src_vocab_size = 32000
 src_model_prefix = 'en'
 src_model_type = 'bpe'
 src_character_coverage = 1
 
-tgt_input_file = './data/corpus.zh'
+tgt_input_file = './data/train/corpus.zh'
 tgt_vocab_size = 32000
 tgt_model_prefix = 'zh'
 tgt_model_type = 'bpe'
