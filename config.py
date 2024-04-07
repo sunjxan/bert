232d1ba849cb22015bb9ea548dade25aa3eca9df
sunjian <420448185@qq.com>
2024-04-07 03:47:48 +0000

    update config.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- config.py
+++ config.py
@@ -12,6 +12,13 @@ tgt_model_prefix = 'zh'
 tgt_model_type = 'bpe'
 tgt_character_coverage = .9995
 
+max_padding = 128
 batch_size = 32
+
+d_model = 512
+n_heads = 8
+n_layers = 6
+d_ff = 2048
+dropout = .1
 
 device = torch.device('cpu')
