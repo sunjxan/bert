2f795849add92a528e7fe8d2fff014ddb529b148
sunjian <420448185@qq.com>
2024-04-09 09:42:41 +0000

    update config.py.
    Signed-off-by: sunjian <420448185@qq.com>

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import torch
 
-src_input_file = './data/train/corpus.en'
+src_train_file = './data/train/corpus.en'
+src_eval_file = './data/eval/corpus.en'
+src_test_file = './data/test/corpus.en'
 src_vocab_size = 32000
 src_model_prefix = 'en'
 src_model_type = 'bpe'
 src_character_coverage = 1
 
-tgt_input_file = './data/train/corpus.zh'
+tgt_train_file = './data/train/corpus.zh'
+tgt_eval_file = './data/eval/corpus.zh'
+tgt_test_file = './data/test/corpus.zh'
 tgt_vocab_size = 32000
 tgt_model_prefix = 'zh'
 tgt_model_type = 'bpe'
@@ -20,5 +24,10 @@ n_heads = 8
 n_layers = 6
 d_ff = 2048
 dropout = .1
+
+base_lr = 1.0
+warmup = 3000
+num_epochs = 8
+accum_iter = 10
 
 device = torch.device('cpu')
